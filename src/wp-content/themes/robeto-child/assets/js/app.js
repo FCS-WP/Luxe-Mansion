54e6aca973194f777794f8a4c8d3fe3bbc8a069d
shin <nhathanh024@gmail.com>
2024-11-14 11:08:22 +0700

    update mega menu

--- src/wp-content/themes/robeto-child/assets/js/app.js
+++ src/wp-content/themes/robeto-child/assets/js/app.js
@@ -1,16 +1,76 @@
-// // import { DisplayLabel } from './components/DisplayLabel';
+$(document).ready(function () {
+  handlerOpenMasterCate();
+  handlerSubLv1();
+  closeMenuByOverlay();
+  closeMenuByClose();
+});
 
-// let Main = {
-//   init: async function () {
+const $html = $("html");
+const $menuShop = $(".menu-shop");
+const $megaMenu = $menuShop.children(".mega-menu");
+const $headerOverlay = $(".header-overlay");
+const $headerClose = $(".header-close");
 
-//     // initialize demo javascript component - async/await invokes some 
-//     //  level of babel transformation 
-//     const displayLabel = new DisplayLabel();
-//     await displayLabel.init();
+function toggleBodyOverflow(isOpen) {
+  if (isOpen) {
+    $html.css("overflow", "hidden");
+  } else {
+    $html.css("overflow", "auto");
+  }
+}
 
-//   }
-// };
+function showMenu() {
+  $megaMenu.addClass("active");
+  $headerOverlay.removeClass("d-none");
+  $headerClose.removeClass("d-none");
+  toggleBodyOverflow(true);
+}
 
+function hideMenu() {
+  $megaMenu.removeClass("active");
+  $headerOverlay.addClass("d-none");
+  $headerClose.addClass("d-none");
+  toggleBodyOverflow(false);
 
-// console.log($('.more-link'));
-// // Main.init();
+  $megaMenu.find("li.active").removeClass("active");
+}
+
+function handlerOpenMasterCate() {
+  $menuShop.on("click", (e) => {
+    // Detect Clicking on $megaMenu
+    if (!$megaMenu.is(e.target) && $megaMenu.has(e.target).length === 0) {
+      e.preventDefault();
+    }
+    showMenu();
+  });
+}
+
+function handlerSubLv1() {
+  $(".mega-menu-main li").each(function (index) {
+    const $subMenuItem = $(this)
+      .children(".sub-menu")
+      .children(".menu-item-has-children");
+    $subMenuItem.on("click", function (e) {
+      // e.preventDefault();
+
+      if (
+        $subMenuItem.is(e.target) &&
+        $subMenuItem.has(e.target).length === 0
+      ) {
+        $(this).toggleClass("active");
+      }
+    });
+  });
+}
+
+function closeMenuByOverlay() {
+  $headerOverlay.on("click", () => {
+    hideMenu();
+  });
+}
+
+function closeMenuByClose() {
+  $headerClose.on("click", () => {
+    hideMenu();
+  });
+}
